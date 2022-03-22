db6266b6b7d86f567f51fb769f1f374cc4523b3b
Mr.Kelvin <locnguyenphuocloc67@gmail.com>
2022-03-22 11:25:31 +0700

    them_GioHangReducer

--- src/redux/reducer/GioHangReducer.js
+++ src/redux/reducer/GioHangReducer.js
@@ -1,1 +1,28 @@
+let stateGioHangDefault = [
+  {
+    maSP: 1,
+    tenSP: "iPhone",
+    giaBan: 1000,
+    soLuong: 1,
+    hinhAnh: "https://i.pravatar.cc?u=1",
+  },
+];
+export const gioHangReducer = (state = stateGioHangDefault, action) => {
+  switch (action.type) {
+    case "THEM_GIO_HANG":
+      {
+        let gioHang = state;
+        let spGH = { ...action.sanPhamClick, soLuong: 1 };
+        let sp = gioHang.find((spGH) => spGH.maSP === action.sanPhamClick.maSP);
+        if (sp) {
+          sp.soLuong += 1;
+        } else {
+          gioHang.push(spGH);
+        }
+        console.log(gioHang);
+        return [...gioHang];
+      }
+  }
+  return state;
 
+};
