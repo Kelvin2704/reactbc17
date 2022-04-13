9b09fa22bb7a63c5023e2d024ef4c8d5602ef166
Mr.Kelvin <locnguyenphuocloc67@gmail.com>
2022-04-13 23:06:25 +0700

    useDispatch_dispatchApiAction

--- src/pages/ReactHook/ReduxHook/ToDoListHook/ToDoListHook.js
+++ src/pages/ReactHook/ReduxHook/ToDoListHook/ToDoListHook.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { getApiArrTaskAction } from "../../../../redux/actions/toDoListActions";
+import { getApiArrTaskAction, addTaskAction, checkDoneTaskAction, deleteTaskApiAction, rejectTaskApiAction } from "../../../../redux/actions/toDoListActions";
 
 export default function ToDoListHook(props) {
+  const [task, setTask] = useState({
+    taskName: "",
+    status: false,
+  });
   //   const [arrTask, setArrTask] = useState([]);
   const { arrTask } = useSelector((state) => state.toDoListReducer);
   const dispatch = useDispatch();
@@ -45,15 +49,55 @@ export default function ToDoListHook(props) {
     dispatch(action);
   }, []);
 
+  const addTaskApi = async (e) => {
+    //khi người dùng submit hoạt enter
+    //xử lý nghiệp vụ thêm task
+    e.preventDefault();
+
+    //sau khi gọi hàm kết quả trả về là (dispatch)
+    const action = addTaskAction(task);
+    dispatch(action);
+  };
+
+  const handleCheckDoneTask = (taskName) => {
+    const action = checkDoneTaskAction(taskName);
+    dispatch(action);
+  };
+
+  const handleDeleteTask = (taskName) => {
+    const action = deleteTaskApiAction(taskName);
+    dispatch(action);
+  };
+
+  const handleRejectTask = (taskName) => {
+    const action = rejectTaskApiAction(taskName);
+    dispatch(action);
+  };
+
+  console.log(task);
   return (
     <div className="container">
       <h3 className="text-center">To do App</h3>
-      <div className="card">
+
+      <form className="card" onSubmit={addTaskApi}>
         <div className="card-header">
           <div className="input-group-prepend mb-3">
-            <span className="input-group-text btn btn-success">Add task</span>
-            <input className="form-control" id="taskName" />
+            <button type="submit" className="input-group-text btn btn-success">
+              Add task
+            </button>
+            <input
+              className="form-control"
+              id="taskName"
+              onChange={(e) => {
+                let { value, id } = e.target;
+                setTask({
+                  ...task,
+                  [id]: value,
+                });
+              }}
+            />
           </div>
+
           <div className="card-body">
             <table className="table">
               <tbody>
@@ -66,11 +110,23 @@ export default function ToDoListHook(props) {
                         <td>{task.taskName}</td>
                         <td>
                           <span className="badge badge-success">Complete</span>
+
+                          <i className="fa fa-undo text-warning" onClick={()=>{
+                            handleRejectTask(task.taskName)
+                          }} style={{ padding: 10, cursor: "pointer", fontSize: 30 }} />
+                          <i
+                            className="fa fa-trash text-danger"
+                            onClick={() => {
+                              handleDeleteTask(task.taskName);
+                            }}
+                            style={{ padding: 10, cursor: "pointer", fontSize: 30 }}
+                          />
                         </td>
                       </tr>
                     );
                   })}
               </tbody>
+
               <tfoot>
                 {/* cac viec chua lam */}
                 {arrTask
@@ -81,6 +137,21 @@ export default function ToDoListHook(props) {
                         <td>{task.taskName}</td>
                         <td>
                           <span className="badge badge-danger">InComplete</span>
+
+                          <i
+                            className="fa fa-check-circle text-success"
+                            onClick={() => {
+                              handleCheckDoneTask(task.taskName);
+                            }}
+                            style={{ padding: 10, cursor: "pointer", fontSize: 30 }}
+                          />
+                          <i
+                            className="fa fa-trash text-danger"
+                            onClick={() => {
+                              handleDeleteTask(task.taskName);
+                            }}
+                            style={{ padding: 10, cursor: "pointer", fontSize: 30 }}
+                          />
                         </td>
                       </tr>
                     );
@@ -89,7 +160,7 @@ export default function ToDoListHook(props) {
             </table>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
--- src/redux/actions/toDoListActions.js
+++ src/redux/actions/toDoListActions.js
@@ -27,3 +27,71 @@ export const getApiArrTaskAction = () => {
     }
   };
 };
+
+export const addTaskAction = (task) => {
+  return async (dispatch) => {
+    try {
+      let result = await axios({
+        url: `http://svcy.myclass.vn/api/ToDoList/AddTask`,
+        method: "POST",
+        data: task, //{taskName:'',status:false}
+      });
+
+      //Gọi api getAllTask
+      console.log("result", result);
+      const action = getApiArrTaskAction();
+      dispatch(action);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const checkDoneTaskAction = (taskName) => {
+  return async (dispatch) => {
+    try {
+      let result = await axios({
+        url: `http://svcy.myclass.vn/api/ToDoList/doneTask?taskName=${taskName}
+        `,
+        method: "PUT",
+      });
+      console.log("thành công", result);
+      const action = getApiArrTaskAction();
+      dispatch(action);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const deleteTaskApiAction = (taskName) => {
+  return async (dispatch) => {
+    try {
+      let result = await axios({
+        url: `http://svcy.myclass.vn/api/ToDoList/deleteTask?taskName=${taskName}
+        `,
+        method: "DELETE",
+      });
+      console.log("thành công", result);
+      const action = getApiArrTaskAction();
+      dispatch(action);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const rejectTaskApiAction = (taskName) => {
+  return async (dispatch) => {
+    try {
+      let result = await axios({
+        url: `http://svcy.myclass.vn/api/ToDoList/rejectTask?taskName=${taskName}`,
+        method: "PUT",
+      });
+      const action = getApiArrTaskAction();
+      dispatch(action);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
